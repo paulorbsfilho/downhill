3da624338dd25b0bb6c7dc352fe9da3f4f15a980
paulorbsfilho <p4ulinho@outlook.com>
2020-10-26 13:44:56 -0300

    Correção física

--- main.gd
+++ main.gd
@@ -1,16 +1,32 @@
 extends Node2D
 
-#front
-var FX1 = 0;
-var FX2 = -10;
-var FY1 = -30;
-var FY2 = -30;
+#1
+var F1X1 = 43;
+var F1Y1 = 0;
 
-#back
-var BX1 = 0;
-var BX2 = 10;
-var BY1 = -30;
-var BY2 = -30;
+var F1X2 = 43;
+var F1Y2 = -25;
+
+#2
+var F2X1 = 43;
+var F2Y1 = 0;
+
+var F2X2 = 43;
+var F2Y2 = 20;
+
+#3
+var B1X1 = -45;
+var B1Y1 = 0;
+
+var B1X2 = -45;
+var B1Y2 = -20;
+
+#4
+var B2X1 = -45;
+var B2Y1 = 0;
+
+var B2X2 = -45;
+var B2Y2 = 20;
 
 func _ready():
 	pass # Replace with function body.
@@ -27,11 +43,8 @@ func _process(delta):
 		$bike/front.angular_velocity = -5
 		$bike/back.angular_velocity = -5
 	if Input.is_action_pressed("ui_left"):
-		$bike/Body.apply_impulse(Vector2(FX1, FY1),Vector2(FX2,FY2))
+		$bike/Body.apply_impulse(Vector2(F1X1, F1Y1),Vector2(F1X2,F1Y2))
+		$bike/Body.apply_impulse(Vector2(B2X1, B2Y1),Vector2(B2X2,B2Y2))
 	if Input.is_action_pressed("ui_right"):
-		$bike/Body.apply_impulse(Vector2(BX1, BY1),Vector2(BX2,BY2))
-		#$bike/back.apply_impulse(Vector2(0, 0),Vector2(0, 250))
-#	if Input.is_action_just_released("ui_left"):
-#		$bike/Body.apply_impulse(Vector2(0, 250),Vector2(0, 250))
-#	if Input.is_action_just_released("ui_right"):
-#		$bike/Body.apply_impulse(Vector2(0, 250),Vector2(0, 250))
+		$bike/Body.apply_impulse(Vector2(B1X1, B1Y1),Vector2(B1X2,B1Y2))
+		$bike/Body.apply_impulse(Vector2(F2X1, F2Y1),Vector2(F2X2,F2Y2))
